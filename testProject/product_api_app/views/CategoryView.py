9692c12ac27253ba3e4460c271badceeb1b9d093
valyalshikovd <770vda@mail.ru>
2025-03-11 08:56:20 +0300

    fix: fix imports

--- testProject/product_api_app/views/CategoryView.py
+++ testProject/product_api_app/views/CategoryView.py
@@ -3,13 +3,13 @@ from rest_framework.views import APIView
 
 from ..service.category_service import get_categories
 from ..service.category_service import create_category
-from ...userapp import permissions
+from userapp import permissions
 
 
 class CategoryView(APIView):
     def get(self, request):
         return get_categories(request)
 
-    @action(permission_classes=[permissions.permissions.IsAdminUser])
+    @action(detail=False, permission_classes=[permissions.permissions.IsAdminUser])
     def post(self, request):
         return create_category(request)
--- testProject/product_api_app/views/ProductView.py
+++ testProject/product_api_app/views/ProductView.py
@@ -3,13 +3,13 @@ from rest_framework.views import APIView
 
 from ..service.product_service import get_products
 from ..service.product_service import create_product
-from ...userapp import permissions
+from userapp import permissions
 
 
 class ProductListView(APIView):
     def get(self, request):
         return get_products(request)
 
-    @action(permission_classes=[permissions.permissions.IsAdminUser])
+    @action(detail=False, permission_classes=[permissions.IsAdmin])
     def post(self, request):
         return create_product(request)
