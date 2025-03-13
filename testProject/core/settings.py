3ba7f9729137303df07647d026f8a201d45b7bd7
valyalshikovd <770vda@mail.ru>
2025-03-13 14:45:47 +0300

    fix: delete django secret key

--- testProject/core/settings.py
+++ testProject/core/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a9gcqfq7+r3mns&@+e8%)ntwrdogd%shm3q9qobexffro$0k8&'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
